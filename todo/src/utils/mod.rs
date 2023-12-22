a62b97fa011f74ea0d86276923337557d8ea149e
Eli <elijames2301@gmail.com>
2023-12-22 18:53:43 -0500

    Write timestamp code

--- todo/src/utils/mod.rs
+++ todo/src/utils/mod.rs
@@ -29,3 +29,10 @@ pub fn get_args() -> structs::Command {
 
     structs::Command { command, arguments }
 }
+
+pub fn get_timestamp() -> String {
+    let now = chrono::Local::now();
+    let timestamp = now.format("%m-%d %H:%M").to_string();
+
+    timestamp
+}
